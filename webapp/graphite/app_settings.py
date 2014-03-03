1792d1d20d8df780af895f7da83d018e0978c418
Pavel Vorobyov <viert@yandex-team.ru>
2014-03-03 15:56:08 +0400

    django_replicated support

--- webapp/graphite/app_settings.py
+++ webapp/graphite/app_settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE_CLASSES = (
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
+  'django_replicated.middleware.ReplicationMiddleware',
 )
 
 ROOT_URLCONF = 'graphite.urls'
