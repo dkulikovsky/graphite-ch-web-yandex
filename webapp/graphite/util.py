855b7bb19609b982c1ffaed2b1058cc0b2522e41
dkulikovsky <dkulikovsky@yandex-team.ru>
2014-03-28 15:41:54 +0400

    fixed remote rendering unsafe pickle

--- webapp/graphite/util.py
+++ webapp/graphite/util.py
@@ -150,8 +150,10 @@ if USING_CPICKLE:
   class SafeUnpickler(object):
     PICKLE_SAFE = {
       'copy_reg': set(['_reconstructor']),
-      '__builtin__': set(['object']),
+      '__builtin__': set(['object', 'list']),
+      'collections': set(['deque']),
       'graphite.intervals': set(['Interval', 'IntervalSet']),
+      'graphite.render.datalib': set(['TimeSeries']),
     }
 
     @classmethod
@@ -174,8 +176,10 @@ else:
   class SafeUnpickler(pickle.Unpickler):
     PICKLE_SAFE = {
       'copy_reg': set(['_reconstructor']),
-      '__builtin__': set(['object']),
+      '__builtin__': set(['object', 'list']),
+      'collections': set(['deque']),
       'graphite.intervals': set(['Interval', 'IntervalSet']),
+      'graphite.render.datalib': set(['TimeSeries']),
     }
 
     def find_class(self, module, name):
