993dbf00e69cb033ddc71e6e030de3f1bd3e5264
antrue <antrue@yandex-team.ru>
2015-04-10 18:22:12 +0300

    added functions: mapSeries, reduceSeries

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -3261,6 +3261,8 @@ SeriesFunctions = {
   'diffSeries' : diffSeries,
   'divideSeries' : divideSeries,
   'yabsStatLag' : yabsStatLag,
+  'mapSeries': mapSeries,
+  'reduceSeries': reduceSeries,
 
   # Series Filter functions
   'mostDeviant' : mostDeviant,
